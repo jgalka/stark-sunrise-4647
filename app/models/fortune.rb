946401f1ec4d535ae33d69ae64f29c8ab7bd23ac
jacekg <andoryt@poczta.onet.pl>
2012-06-03 12:13:03 +0200

    na Heroku

---
+++ app/models/fortune.rb
@@ -0,0 +1,4 @@
+class Fortune < ActiveRecord::Base
+    cattr_reader :per_page
+    @@per_page = 5
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortunka::Application.config.secret_token = '782f553e5052c1e9104dfef183784f40113c459abfdd3791c25bfd07a014ad773301db48f0d8bebea5abacec4d42252b490c8ef3a848362f476f292220e7d236'
